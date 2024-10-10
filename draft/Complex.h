68ba910a2e418c6810117e0add32e3996d1eaf25
joson <joson0223@163.com>
2024-10-10 10:43:07 +0800

    2

---
+++ draft/Complex.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <cmath>
+
+class ostream;
+class Complex;
+Complex& __doapl(Complex* ths, const Complex& r);
+
+class Complex
+{
+};
+
---
+++ draft/main.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+#include "Complex.h"
+using namespace std;
+
+int main() {
+	
+	return 0;
+}
