9af8ff96dc72aae38a5910d540b1d89223efb05e
joson <joson0223@163.com>
2024-10-12 16:05:56 +0800

    add

--- draft/String.h
+++ draft/String.h
@@ -1,7 +1,55 @@
 #pragma once
+#include <cstring>
+#include <iostream>
+
 class String
 {
 public:
-	String(const char * cstr = nullptr);
+	String(const char *cstr = 0);
+	String(const String&);
+	String& operator=(const String&);
+	~String();
+	char* get_c_str() const { return m_data;}
+private:
+	char *m_data;
 };
 
+
+inline
+String::String(const char *cstr) {
+	if (cstr) {
+		m_data = new char[strlen(cstr) + 1];
+		strcpy(m_data, cstr);
+	} else {
+		m_data = new char[1];
+		*m_data = '\0';
+	}
+}
+
+inline 
+String::~String() {
+	delete[] m_data;
+}
+
+inline
+String::String(const String& other) {
+	m_data = new char[strlen(other.m_data) + 1];
+	strcpy(m_data, other.m_data);
+}
+
+inline String&
+String::operator = (const String& other) {
+	if (this == &other) {
+		return *this;
+	}
+
+	delete[] m_data;
+	m_data = new char[strlen(other.m_data) + 1];
+	strcpy(m_data, other.m_data);
+	return *this;
+}
+
+inline std::ostream&
+operator<<(std::ostream& os, const String& str) {
+	return os << str.get_c_str();
+}
--- draft/main.cpp
+++ draft/main.cpp
@@ -1,14 +1,19 @@
-﻿#include <iostream>
-#include "Complex.h"
+﻿#include "Complex.h"
 #include "String.h"
 using namespace std;
 
 int main()
 {
-    Complex c1;
-    Complex c2;
-    cout << (c1 == c2) << endl;
-    cout << (c1 == 0) << endl;
-    cout << (5 == c1) << endl;
+    //Complex c1;
+    //Complex c2;
+    //cout << (c1 == c2) << endl;
+    //cout << (c1 == 0) << endl;
+    //cout << (5 == c1) << endl;
+
+    String s1();
+    String s2("hello");
+    cout << s2 << endl;
+    String *p = new String("hello");
+    delete p;
 }
 
